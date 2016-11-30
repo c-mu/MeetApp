2bf7955aac64f8819d5e83b20702aa51be7d43e3
Christoph Murczek <cmu@outlook.at>
2016-11-30 01:39:29 +0100

    save and retrieve long lived accesstokens from appsettings

--- src/MeetApp/AuthenticationManager.cs
+++ src/MeetApp/AuthenticationManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Security.Authentication.Web;
+using Windows.Storage;
 
 namespace MeetApp
 {
@@ -23,16 +24,28 @@ namespace MeetApp
 
         public async Task Authenticate()
         {
-            var redirectUrl = "https://www.meetup.com";
-            var requestUri = new Uri($"https://secure.meetup.com/oauth2/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectUrl}");
-            var callbackUri = new Uri(redirectUrl);
-            var authenticationResponse = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, requestUri, callbackUri);
-            IsAuthenticated = authenticationResponse.ResponseStatus == WebAuthenticationStatus.Success ? true : false;
-            if (IsAuthenticated)
+            if (string.IsNullOrWhiteSpace(ApplicationData.Current.LocalSettings.Values["AccessToken"].ToString()) &&
+                (DateTimeOffset)ApplicationData.Current.LocalSettings.Values["ExpiresAt"] < DateTimeOffset.Now.AddHours(1))
             {
-                var responseQuery = new Uri(authenticationResponse.ResponseData).Fragment.Substring(1);
-                var queryParams = responseQuery.Split('&');
-                AccessToken = queryParams.First(qp => qp.StartsWith("access_token", StringComparison.OrdinalIgnoreCase)).Split('=')[1];
+                var redirectUrl = "https://www.meetup.com";
+                var requestUri = new Uri($"https://secure.meetup.com/oauth2/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectUrl}&scope=ageless");
+                var callbackUri = new Uri(redirectUrl);
+                var authenticationResponse = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, requestUri, callbackUri);
+                IsAuthenticated = authenticationResponse.ResponseStatus == WebAuthenticationStatus.Success ? true : false;
+                if (IsAuthenticated)
+                {
+                    var responseQuery = new Uri(authenticationResponse.ResponseData).Fragment.Substring(1);
+                    var queryParams = responseQuery.Split('&');
+                    AccessToken = queryParams.First(qp => qp.StartsWith("access_token", StringComparison.OrdinalIgnoreCase)).Split('=')[1];
+                    var validFor = queryParams.First(qp => qp.StartsWith("expires_in", StringComparison.OrdinalIgnoreCase)).Split('=')[1];
+                    ApplicationData.Current.LocalSettings.Values.Add("AccessToken", AccessToken);
+                    ApplicationData.Current.LocalSettings.Values.Add("ExpiresAt", DateTimeOffset.Now.AddSeconds(Convert.ToDouble(validFor)));
+                } 
+            }
+            else
+            {
+                IsAuthenticated = true;
+                AccessToken = ApplicationData.Current.LocalSettings.Values["AccessToken"].ToString();
             }
         }
 
